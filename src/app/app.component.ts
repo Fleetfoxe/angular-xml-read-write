0397b47796c21b013d9d377c5d9b3b83f86bd357
Peter <p.akesson@gmail.com>
2023-08-17 10:48:30 +0200

    added import-all from other example file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import * as xml2js from 'xml2js';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public xmlItems: any;
+
+  constructor(private http:HttpClient) {
+
+    this.loadXML();
+  }
+
+  //getting data function
+  loadXML()
+  {
+    /*Read Data*/
+    this.http.get('assets/users.xml',
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'text/xml')
+          .append('Access-Control-Allow-Methods', 'GET')
+          .append('Access-Control-Allow-Origin', '*')
+          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
+        responseType: 'text'
+      })
+      .subscribe((data) => {
+        this.parseXML(data)
+          .then((data) => {
+            this.xmlItems = data;
+          });
+      });
+    /*Read Data*/
+  }
+
+  //store xml data into array variable
+  parseXML(data: any) {
+    return new Promise(resolve => {
+      var k: string | number,
+        arr :any[] = [],
+        parser = new xml2js.Parser(
+          {
+            trim: true,
+            explicitArray: true
+          });
+      parser.parseString(data, function (err:any, result:any) {
+        var obj = result.Employee;
+        for (k in obj.emp) {
+          var item = obj.emp[k];
+          arr.push({
+            id: item.id[0],
+            name: item.name[0],
+            email: item.email[0],
+
+          });
+        }
+        resolve(arr);
+      });
+    });
+  }
 }

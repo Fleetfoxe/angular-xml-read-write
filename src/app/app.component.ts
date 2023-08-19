64070fd42f67103ce53be230f5f9fd8940f19ab4
Peter <p.akesson@gmail.com>
2023-08-19 08:52:58 +0200

    small change to design. formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,39 +21,8 @@ export class AppComponent {
 
   constructor(private http:HttpClient, private sanitizer: DomSanitizer) { }
 
-  parseXML = (data: any) => {
-    var parserSearchId = this.searchId;
-    var valueToFind: string;
-    return new Promise(resolve => {
-      var k: string | number,
-        arr :any[] = [],
-        parser = new xml2js.Parser(
-          {
-            trim: true,
-            explicitArray: true
-          });
-      parser.parseString(data, function (err:any, result:any) {
-        const listOftransUnits :[] = result.root.file[0].body[0][`trans-unit`];
-        listOftransUnits.forEach((element) => {
-          var metaData :any = (element[`$`])
-          if (metaData.id == parserSearchId) {
-            valueToFind = element[`target`][0]
-          }
-        });
-        resolve(arr);
-      });
-      this.targetText = valueToFind;
-      this.updateFileData();
-      this.showDownloadButton = true;
-    });
-  }
 
-  updateFileData() {
-    const blob = new Blob([this.targetText], { type: 'application/octet-stream' });
-    this.savedFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
-  }
-
-   selectFile(event:any) {
+  selectFile(event:any) {
     var file: File = event.target.files[0];
     var fileExtension:string = file.name.substring(file.name.lastIndexOf('.') + 1);
 
@@ -71,11 +40,47 @@ export class AppComponent {
       this.importFileData = reader.result;
     };
     reader.readAsText(file);
-    this.showParseButton = true;
-   }
+    //this.showParseButton = true;
+  }
+
+  parseXML = (data: any) => {
+    var parserSearchId = this.searchId;
+    var valueToFind: string;
+    return new Promise(resolve => {
+      var k: string | number,
+        arr :any[] = [],
+        parser = new xml2js.Parser(
+          {
+            trim: true,
+            explicitArray: true
+          });
+      parser.parseString(data, function (err:any, result:any) {
+        const listOfTransunits :[] = result.root.file[0].body[0][`trans-unit`];
+        listOfTransunits.forEach((element) => {
+          const metaData :any = (element[`$`])
+          if (metaData.id == parserSearchId) {
+            valueToFind = element[`target`][0]
+          }
+        });
+        resolve(arr);
+      });
+      this.targetText = valueToFind;
+      this.updateFileData();
+      this.showDownloadButton = true;
+    });
+  }
+
+  updateFileData() {
+    const blob = new Blob([this.targetText], { type: 'application/octet-stream' });
+    this.savedFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
 
    activateParsing() {
      this.parseXML(this.importFileData)
+   }
+
+   showParsing () {
+    this.showParseButton = true;
    }
 
 }
